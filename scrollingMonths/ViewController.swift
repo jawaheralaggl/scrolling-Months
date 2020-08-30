f4a4102c606b7524c4744d9cbe3bc1283f403862
jawaheralaggl <jawaheralaggl@gmail.com>
2020-08-30 18:18:19 +0300

    Initial Commit

---
+++ scrollingMonths/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scrollingMonths
+//
+//  Created by Jawaher Alagel on 8/30/20.
+//  Copyright © 2020 Jawaher Alaggl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
